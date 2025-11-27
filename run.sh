dfa837ca958b7c6349fe4b2d8dc970ff3e19767c
Martin Billinger-Finke <mbillingr@users.noreply.github.com>
2025-11-27 08:42:04 +0100

    prepare framework

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Usage: run.sh <day> <part>
+
+HERE=`pwd`
+
+DAY=$1
+PT=$2
+YEAR=${3:-2025}
+YSRC=$3
+
+cat ~/aoc-data/$YEAR/input-$DAY.txt | ./interpreter $HERE/src/${YSRC}/day$DAY-part$PT.ml
+
