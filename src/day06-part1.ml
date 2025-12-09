dd07288318aee628c7e50723e34e0f52a6ee5059
Martin Billinger-Finke <m.billinger-finke@daphos.ai>
2025-12-09 20:55:02 +0100

    refactor day 6, part 1

--- src/day06-part1.ml
+++ src/day06-part1.ml
@@ -10,31 +10,23 @@ import "libs/parsing.ml";
 let parse_line = parsing.seplist(parsing.repeat_min(parsing.ws, 1), parsing.num, 0);
 let parse_ops = parsing.seplist(parsing.repeat_min(parsing.ws, 1), parsing.any_char, 0);
 
-let zs = vec.iter option.unwrap parsing.parse(io.read_expected_line {}, parse_line);
-let ys = vec.iter option.unwrap parsing.parse(io.read_expected_line {}, parse_line);
-let xs = vec.iter option.unwrap parsing.parse(io.read_expected_line {}, parse_line);
-let ws = vec.iter option.unwrap parsing.parse(io.read_expected_line {}, parse_line);
+// two definitions to avoid writing type annotations :)
+let parse1 = fun parser -> vec.iter option.unwrap parsing.parse(io.read_expected_line {}, parser);
+let parse2 = fun parser -> vec.iter option.unwrap parsing.parse(io.read_expected_line {}, parser);
 
-let ops = vec.iter option.unwrap parsing.parse(io.read_expected_line {}, parse_ops);
+let ws = parse1 parse_line;
+let xs = parse1 parse_line;
+let ys = parse1 parse_line;
+let zs = parse1 parse_line;
+let ops = parse2 parse_ops;
+let tasks = iter.zip(ops, iter.zip(iter.zip(ws, xs), iter.zip(ys, zs)));
 
+let compute = fun (op, ((w, x), (y, z))) -> 
+  if op == "+" then w + x + y + z else
+  if op == "*" then w * x * y * z else
+  panic op;
 
-let result = 
-  let vars = {mut zs; mut ys; mut xs; mut ws; mut ops; mut total=0} in
-    loop match zs {} with
-      | `None _ -> `Break vars.total
-      | `Some z -> (
-        let y = option.unwrap ys {};
-        let x = option.unwrap xs {};
-        let w = option.unwrap ws {};
-        let op = option.unwrap ops {};
-        vars.total <- vars.total + (
-          if op == "+" then w + x + y + z else
-          if op == "*" then w * x * y * z else
-          panic op);
-        `Continue {}  
-      );
-    
-
+let result = int.sum iter.map(compute, tasks);
 
 io.write_line ("Day 6, Part 1: " ^ int.to_str(result));
 
